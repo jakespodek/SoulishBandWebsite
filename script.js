a80362031125f3c1acdd7f20e4e006a456946413
Jake Spodek <68516673+jakespodek@users.noreply.github.com>
2024-06-14 14:55:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 const app = {};
+app.sheetID = '1joDvSsXHNv70XpwPRGjgNn9pDMEfnbvIB_ogXiag8tA';
+app.base = `https://docs.google.com/spreadsheets/d/${app.sheetID}/gviz/tq?`;
+app.sheetName = 'shows';
+app.query = encodeURIComponent('Select *');
+app.url = `${app.base}&sheet=${app.sheetName}&tq=${app.query}`;
 
-app.upcomingShows = [
-    {
-        date: "April 28, 2022",
-        city: "Toronto, ON",
-        venue: "Lee's Palace",
-    },
-    // {
-    //     date: "May 11, 2022",
-    //     city: "Example, ON",
-    //     venue: "Cool Place",
-    // },
-];
+app.fetchShows = async () => {
+    try {
+        const response = (await fetch(app.url)).text();
+        const data = JSON.parse((await response).slice(47, -2)).table;
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
 
-app.displayShows = () => {
+app.renderShows = async () => {
     const showsList = document.getElementById("showsList");
-
-    app.upcomingShows.forEach(show =>
+    const data = await app.fetchShows();
+    const rows = data.rows;
+    rows.forEach(row => {
         showsList.innerHTML += `
             <li class="mb-12">
-                <div class="flex justify-between items-center">
-                    <p>${show.date}</p>
-                    <div>
-                        <p>${show.city}</p>
-                        <p>${show.venue}</p>
-                    </div>
+                <div class="flex justify-between">
+                    <p>${row.c[0].f}</p>
+                    <p>${row.c[1].v}</p>
+                    <p>${row.c[2].v}</p>
+                    <p><a href="${row.c[3].v}" target="_blank">Tickets</a></p>
                 </div>
             </li>
         `
-    );
-};
+    })
+}
 
 app.init = () => {
-    app.displayShows();
+    app.renderShows();
 };
 
 app.init();
