e45e0de67b4382d0a0850a6f001d392f24c63061
Jake Spodek <68516673+jakespodek@users.noreply.github.com>
2024-06-17 17:18:51 -0400

    updated links

--- src/script.js
+++ src/script.js
@@ -1,9 +1,24 @@
 const app = {};
+
 app.sheetID = '1joDvSsXHNv70XpwPRGjgNn9pDMEfnbvIB_ogXiag8tA';
 app.base = `https://docs.google.com/spreadsheets/d/${app.sheetID}/gviz/tq?`;
 app.sheetName = 'shows';
 app.query = encodeURIComponent('Select *');
 app.url = `${app.base}&sheet=${app.sheetName}&tq=${app.query}`;
+
+app.links = [
+    {name: 'spotify', url: 'https://open.spotify.com/artist/1urqcRSioOrL86IoxS1J5P', isActive: true},
+    {name: 'applemusic', url: 'https://music.apple.com/ca/artist/soulish/1500136109', isActive: false},
+    {name: 'soundcloud', url: 'https://soundcloud.com/soulish-band/', isActive: false},
+    {name: 'bandcamp', url: 'https://soulishband.bandcamp.com/', isActive: false},
+    {name: 'facebook', url: 'https://www.facebook.com/pages/category/Musician-band/Soulish-378186969413040/', isActive: true},
+    {name: 'instagram', url: 'https://www.instagram.com/soulish_inc/', isActive: true},
+    {name: 'tiktok', url: 'https://www.tiktok.com/@_soulish/', isActive: false},
+    {name: 'youtube', url: 'https://www.youtube.com/channel/UCQElp0USJD3VEh56v6cXkpQ/videos', isActive: true},
+    {name: 'etsy', url: 'https://www.etsy.com/ca/shop/SoulishStore/', isActive: false}
+]
+
+app.links.forEach(link => {if(link.isActive) document.getElementById(link.name).href = link.url;})
 
 app.fetchShows = async () => {
     try {
