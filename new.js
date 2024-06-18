93b6543b680adacb9772324f3cddd521bb5a7733
Jake Spodek <68516673+jakespodek@users.noreply.github.com>
2024-06-18 17:23:16 -0400

    update

---
+++ new.js
@@ -0,0 +1,56 @@
+const app = {};
+app.sheetID = '1joDvSsXHNv70XpwPRGjgNn9pDMEfnbvIB_ogXiag8tA';
+app.base = `https://docs.google.com/spreadsheets/d/${app.sheetID}/gviz/tq?`;
+app.query = encodeURIComponent('Select *');
+
+app.fetchData = async (sheet) => {
+    const url = `${app.base}&sheet=${sheet}&tq=${app.query}`;
+    try {
+        const response = (await fetch(url)).text();
+        const data = JSON.parse((await response).slice(47, -2)).table;
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+app.getShows = async () => {
+    const showsList = document.getElementById("showsList");
+    const data = await app.fetchData('shows');
+    const shows = data.rows;
+    shows.forEach(row => {
+        showsList.innerHTML += `
+            <li class="mb-12">
+                <div class="flex justify-between">
+                    <p>${row.c[0].f}</p>
+                    <p>${row.c[1].v}</p>
+                    <p>${row.c[2].v}</p>
+                    <p><a href="${row.c[3].v}" target="_blank">Tickets</a></p>
+                </div>
+            </li>
+        `
+    })
+};
+
+app.getLinks = async () => {
+    const data = await app.fetchData('links');
+    const links = data.rows.filter(row => row.c[1].v);
+    links.forEach(link => {
+        document.getElementById(link.c[0].v).href = link.c[2].v;
+    })
+}
+
+app.getBio = async () => {
+    const about = document.getElementById("about");
+    const data = await app.fetchData('bio');
+    const bio = data.rows;
+    bio.forEach(p => { about.innerHTML += `<p class="mt-4">${p.c[1].v}</p>` })
+}
+
+app.init = () => {
+    app.getShows();
+    app.getLinks();
+    app.getBio();
+};
+
+app.init();
