91dd0a7be25647d148088d366cbab3ab380e99f7
Jake Spodek <68516673+jakespodek@users.noreply.github.com>
2024-10-25 12:13:38 -0400

    new single

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
       colors: {
         pclpurp: '#b628fa',
         soulblue: '#6bd6e2',
-        uturn: '#fc5102'
+        uturn: '#fc5102',
+        wickedgrey: '#b9b6b5'
       }
     },
   },
